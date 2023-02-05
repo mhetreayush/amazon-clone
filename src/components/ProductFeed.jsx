57ba9609c247b8e33efcd15d7e107ed308753d09
Ayush <mhetreayush1719@gmail.com>
2023-02-05 16:08:55 +0530

    eslint fixes

--- src/components/ProductFeed.jsx
+++ src/components/ProductFeed.jsx
@@ -3,8 +3,8 @@ import Product from "./Product";
 const ProductFeed = ({ products }) => {
   return (
     <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-0 md:-mt-52 mx-auto">
-      {products.slice(0, 4).map((product, idx) => (
-        <Product key={idx} {...product} />
+      {products.slice(0, 4).map((product) => (
+        <Product key={product.id} {...product} />
       ))}
 
       <img
@@ -14,13 +14,13 @@ const ProductFeed = ({ products }) => {
       />
 
       <div className="md:col-span-2">
-        {products.slice(4, 5).map((product) => (
-          <Product {...product} />
+        {products.slice(4, 5).map((product, idx) => (
+          <Product {...product} key={product.id} />
         ))}
       </div>
 
       {products.slice(5, products.length).map((product) => (
-        <Product {...product} />
+        <Product key={product.id} {...product} />
       ))}
     </div>
   );
